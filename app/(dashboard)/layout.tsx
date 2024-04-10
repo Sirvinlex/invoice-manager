692c2f75202f37d096e34bc9a41a5f10d9955d79
Alex <vinlex3030@gmail.com>
2024-04-10 12:51:06 +0100

    starting files

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from "react";
+
+import React from 'react'
+
+const layout = ({ children }: PropsWithChildren) => {
+  return (
+    <div>children</div>
+  )
+}
+
+export default layout;

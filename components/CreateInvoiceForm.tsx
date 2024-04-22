209a5f4045968e6539fca6423bfdd0f9427b0c40
Alexander Vincent <vinlex3030@gmail.com>
2024-04-22 21:44:57 +0100

    updating create invoice form

--- components/CreateInvoiceForm.tsx
+++ components/CreateInvoiceForm.tsx
@@ -1,19 +1,30 @@
 "use client"
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, } from 'react';
 import { Trash } from 'lucide-react';
 import { Input } from './ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue, } from "./ui/select";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow, } from "@/components/ui/table"
 
+
 import { Button } from './ui/button';
 
-const ItemList = ({itemId}: {itemId: any}) => {
+const ItemList = ({setTotalAmount, totalAmount, itemId,}: {setTotalAmount: any,totalAmount: any, itemId: any}) => {
   // "use client"
   const [price, setPrice] = useState(0);
   const [quantity, setQuantity] = useState(0);
   const [total, setTotal] = useState(0);
+  const [prevTotal, setPrevTotal] = useState(0);
 
   // let ref = useRef(0);
+  useEffect(() =>{
+    // console.log(totalAmount, 'total amount')
+    // setTotalAmount(totalAmount - prevTotal); 
+    setTotalAmount((prevState: any) => prevState - prevTotal); 
+    // setTotalAmount(totalAmount + total);
+    setTotalAmount((prevState: any) => prevState + total);
+    // setAge(a => a + 1)
+  }, [total]);
+
   return (
     <div className='flex justify-between mb-4'>
       <div className='w-4/12 flex flex-col'>
@@ -24,8 +35,8 @@ const ItemList = ({itemId}: {itemId: any}) => {
         <label htmlFor='quantity'>Qty</label>
         <Input type='text' name='quantity' id='quantity' value={quantity} onChange={(e) => {
           setQuantity(Number(e.target.value));
-          // ref.current = Number(e.target.value) * price;
-          setTotal(Number(e.target.value) * price)
+          setPrevTotal(total);
+          setTotal(Number(e.target.value) * price);  
         }
         }/>
       </div>
@@ -33,8 +44,8 @@ const ItemList = ({itemId}: {itemId: any}) => {
         <label htmlFor='price'>Price</label>
         <Input type='text' name='price' id='price' value={price} onChange={(e) => {
           setPrice(Number(e.target.value));
-          setTotal(Number(e.target.value) * quantity)
-          // ref.current = Number(e.target.value) * quantity;
+          setPrevTotal(total);
+          setTotal(Number(e.target.value) * quantity);
         }
         }/>
       </div>
@@ -57,20 +68,18 @@ const ItemList = ({itemId}: {itemId: any}) => {
 
 
 const CreateInvoiceForm = () => {
-  const [total, setTotal] = useState(0);
+  const [totalAmount, setTotalAmount] = useState(0);
   const [items, setItems] = useState<any[]>([]);
   const [itemInput, setItemInput] = useState<any[]>([]);
   const [showBtn, setShowBtn] = useState(false);
+  const [currency, setCurrency] = useState('');
   
-  const handleDelItem = (id: any) =>{
-    items.map((item, i) =>{
-      console.log(item[1])
-    })
-  } 
+  const currencyArr = ['USD $','NGN ₦', 'AUD $', 'CAD $'];
+
   const handleAddItem = () =>{
     setShowBtn(true);
     const itemId = Math.ceil(Math.random() * 10) + new Date().getTime();
-    setItems([...items, [<ItemList itemId={itemId} />, itemId]]);
+    setItems([...items, [<ItemList setTotalAmount={setTotalAmount} totalAmount={totalAmount} itemId={itemId} />, itemId]]);
   };
 
   const handleDeleteItem = (e: any) => {
@@ -107,17 +116,51 @@ const CreateInvoiceForm = () => {
     };
     
     setItemInput([...itemInput, ...itemInputArr]);
-    
-    let totalAmount = 0;
-    totalArr.map((item) => totalAmount = totalAmount + Number(item));
-    setTotal((prevState) => prevState + totalAmount);  
+      
     setShowBtn(false);
     setItems([]);
   }
 
+  const handleSubmit = (e: any) =>{
+    e.preventDefault();
+    // setItemInput([]);
+    const formData = new FormData(e.currentTarget);
+    const totalArr = formData.getAll('total');
+    const itemArr = formData.getAll('item');
+    const quantityArr = formData.getAll('quantity');
+    const priceArr = formData.getAll('price');
+
+    const itemInputArr = [];
+
+    let count = 0;
+    while (count < totalArr.length){
+      itemInputArr.push([itemArr[count], quantityArr[count], priceArr[count], totalArr[count],])
+      count = count + 1;
+    };
+    
+    const itemInputArrFinal = [...itemInput, ...itemInputArr];
+
+    const street = formData.get('street');
+    const city = formData.get('city');
+    const postCode = formData.get('post-code');
+    const country = formData.get('country');
+    const name = formData.get('name');
+    const email = formData.get('email');
+    const clientStreet = formData.get('client-street');
+    const clientCity = formData.get('client-city');
+    const clientPostCode = formData.get('client-post-code');
+    const clientCountry = formData.get('client-country');
+    const invoiceDate = formData.get('invoice-date');
+    const dueDate = formData.get('due-date');  
+    const paymentTerm = formData.get('payment-term');  
+    const description = formData.get('description');  
+    const others = formData.get('others');  
+
+  };
+
   return (
     <>
-    <form className=' px-8 py-9'>
+    <form onSubmit={handleSubmit} className=' px-8 py-9'>
       <p>Bill from</p>
       <label htmlFor='street'>Street Address</label>
       <Input type='text' name='street' id='street'/>
@@ -135,7 +178,7 @@ const CreateInvoiceForm = () => {
           <Input type='text' name='country' id='country'/>
         </div>
       </div>
-      <p className='mt-6'>Bill from</p>
+      <p className='mt-6'>Bill To</p>
       <label htmlFor='name'>Client's Name</label>
       <Input className='mb-4' type='text' name='name' id='name'/>
       <label htmlFor='email'>Client's Email</label>
@@ -168,7 +211,24 @@ const CreateInvoiceForm = () => {
         </div>
         <div className='flex flex-col'>
           <label htmlFor='due-date'>Currency</label>
-          <Input placeholder='Select input for currency selection will be here' type='text' name='due-date' id='due-date'/>
+          {/* <Select >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue onChange={(e: any) => console.log(e.target.value) } placeholder="Select Currency" />
+          </SelectTrigger >
+          <SelectContent>
+            {currencyArr.map((currency: any, i: any) => <SelectItem key={i} value={currency}>{currency}</SelectItem>)}
+          </SelectContent>
+        </Select> */}
+        <select
+      value={currency} // ...force the select's value to match the state variable...
+      onChange={e => setCurrency(e.target.value)} // ... and update the state variable on any change!
+    >
+      {currencyArr.map((currency: any, i: any) => <option  key={i} value={currency}>{currency.split(' ')[0]} ({currency.split(' ')[1]})</option>)}
+      
+      
+    </select>
+
+          {/* <Input placeholder='Select input for currency selection will be here' type='text' name='due-date' id='due-date'/> */}
         </div>
       </div>
       <label htmlFor='payment-term'>Payment Terms</label>
@@ -176,48 +236,31 @@ const CreateInvoiceForm = () => {
       <label htmlFor='description'>Project Description</label>
       <Input className='mb-4' type='text' name='description' id='description'/>
       <label htmlFor='others'>Additional Information</label>
-      <Input placeholder='Account number, wallet address and other' type='text' name='others' id='others'/>
+      <Input placeholder='Other relevant information' type='text' name='others' id='others'/>
+
+      {items.map((item: any ) =>{
+        return <>
+          {showBtn? (
+            <div key={item[1]} className='flex'><div>{item[0]}</div><Button data-id={item[1]} variant='ghost' onClick={handleDeleteItem}><Trash/></Button></div>
+      
+          ) : null}
+        </>
+        })}
+
+      <Button variant='ghost' className='w-full mb-4 text-green-500' type='button' onClick={handleAddItem}>+Add New Item</Button>
+      <Button type='submit'>Submit</Button>
     </form>
 
-    {!showBtn && itemInput.length < 1 ? <p className='mt-6 text-center'>You have not added any item to this invoice</p> : null}
-    {!showBtn && itemInput.length > 0 ? (
-            <Table>  
-              <TableCaption>A list of your invoice items.</TableCaption>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="w-[100px]">Item Name</TableHead>
-                  <TableHead>Quantity</TableHead>
-                  <TableHead>Price</TableHead>
-                  <TableHead className="text-right">Total</TableHead>
-                </TableRow>
-              </TableHeader>
-              {/* {itemInput.map((item, i) =>{ */}
-                {/* return ( */}
-                  <TableBody>
-                    {itemInput.map((item, i) =>{
-                      return (
-                        <TableRow key={i}>
-                          <TableCell className="font-medium">{item[0]}</TableCell>
-                          <TableCell>{item[1]}</TableCell>
-                          <TableCell>{item[2]}</TableCell>
-                          <TableCell className="text-right">{item[3]}</TableCell>
-                        </TableRow>
-                      )
-                    })}
-                    <TableRow>
-                          <TableCell className="font-medium"></TableCell>
-                          <TableCell></TableCell>
-                          <TableCell></TableCell>
-                          <TableCell className="text-right">Total: {total}</TableCell>
-                        </TableRow>
-                  </TableBody>
-                
-              
-            </Table>
-          
-      ) : null}
+    
+    <p className='pb-10'>Total: {currency.split(' ')[1] ? currency.split(' ')[1] : '$'} {totalAmount}</p>
+    </>
+  )
+}
 
-    <form className=' px-8 py-9' onSubmit={saveChanges}>
+export default CreateInvoiceForm;
+
+
+{/* <form className=' px-8 py-9' onSubmit={saveChanges}>
       
       {items.map((item: any ) =>{
         return <>
@@ -228,18 +271,13 @@ const CreateInvoiceForm = () => {
         </>
         })}
 
-      <Button className='w-full mb-4' type='button' onClick={handleAddItem}>+Add New Item</Button>
+      <Button variant='ghost' className='w-full mb-4 text-green-500' type='button' onClick={handleAddItem}>+Add New Item</Button>
       
       {showBtn ? (
         <div className='grid gap-6 grid-cols-2'>
-          <Button type='submit' className='bg-green-500 hover:bg-green-400'>Save Changes</Button>
+          <Button type='submit' className='bg-green-500 hover:bg-green-400'>Save Items</Button>
           <Button className='bg-amber-500 hover:bg-amber-400' type='button' onClick={handleClearItems}>Cancel</Button>
         </div>
       ) : null}
       
-    </form>
-    </>
-  )
-}
-
-export default CreateInvoiceForm;
+    </form> */}
